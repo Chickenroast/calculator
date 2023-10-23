d937657678398651d9e6164e35be66f9463f71a2
Julia Michel T. <juliamicheltex@gmail.com>
2023-10-23 22:16:44 +0200

    problemes de deploy js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ function calculer() {
   let b = eval(a);
   document.getElementById("output").value = b;
 }
-//fonction qui affiche la valeur
+
 function afficher(val) {
   document.getElementById("output").value += val;
 }
-//fonction qui efface l'écran
+
 function effacer() {
   document.getElementById("output").value = "";
 }
--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/calculator/",
+  build: {
+    rollupOptions: {
+      output: {
+        input: "index.html",
+        entryFileNames: "main.js", // Personnalisez le nom du fichier ici
+      },
+    },
+  },
 });
