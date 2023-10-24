c9e57573e79687985654362efe452f7be6018688
Julia Michel T. <juliamicheltex@gmail.com>
2023-10-24 21:56:21 +0200

    main js réparé projet sauvé

--- main.js
+++ main.js
@@ -1,58 +1,103 @@
-function calculer() {
-  let a = document.getElementById("output").value;
-  let b = eval(a);
-  document.getElementById("output").value = b;
+// L'enfer des déclarations
+
+document.getElementById("btn1").addEventListener("click", function () {
+  appendToOutput("1");
+});
+
+document.getElementById("btn2").addEventListener("click", function () {
+  appendToOutput("2");
+});
+
+document.getElementById("btn3").addEventListener("click", function () {
+  appendToOutput("3");
+});
+
+document.getElementById("btnPlus").addEventListener("click", function () {
+  appendToOutput("+");
+});
+
+document.getElementById("btn4").addEventListener("click", function () {
+  appendToOutput("4");
+});
+
+document.getElementById("btn5").addEventListener("click", function () {
+  appendToOutput("5");
+});
+
+document.getElementById("btn6").addEventListener("click", function () {
+  appendToOutput("6");
+});
+
+document.getElementById("btnMinus").addEventListener("click", function () {
+  appendToOutput("-");
+});
+
+document.getElementById("btn7").addEventListener("click", function () {
+  appendToOutput("7");
+});
+
+document.getElementById("btn8").addEventListener("click", function () {
+  appendToOutput("8");
+});
+
+document.getElementById("btn9").addEventListener("click", function () {
+  appendToOutput("9");
+});
+
+document.getElementById("btnMultiply").addEventListener("click", function () {
+  appendToOutput("*");
+});
+
+document.getElementById("btn0").addEventListener("click", function () {
+  appendToOutput("0");
+});
+
+document.getElementById("btnClear").addEventListener("click", function () {
+  clearOutput();
+});
+
+document.getElementById("btnEquals").addEventListener("click", function () {
+  calculate();
+});
+
+document.getElementById("btnDivide").addEventListener("click", function () {
+  appendToOutput("/");
+});
+
+document.getElementById("output").addEventListener("click", function () {
+  appendToOutput("");
+});
+
+document.getElementById("output2").addEventListener("mouseover", function () {
+  moveTd();
+});
+
+// Nos fonctions
+
+function appendToOutput(value) {
+  document.getElementById("output2").value += value;
 }
 
-function afficher(val) {
-  document.getElementById("output").value += val;
+function clearOutput() {
+  document.getElementById("output2").value = "";
 }
 
-function effacer() {
-  document.getElementById("output").value = "";
+function calculate() {
+  const output = document.getElementById("output2").value;
+  try {
+    const result = eval(output);
+    document.getElementById("output2").value = result;
+  } catch (error) {
+    document.getElementById("output2").value = "Erreur";
+  }
 }
 
-let isMoving = false;
-let isMovingTL = false;
-let isMovingTY = false;
+// FONCTION FOU LE BORDEL lel
+let isMovingTd = false;
 
 function moveTd() {
-  if (!isMoving) {
-    isMoving = true;
-    const button = document.getElementById("move");
-    const originalPosition = button.style.transform;
-
-    button.style.transform = "translateY(100px)";
-
-    setTimeout(() => {
-      button.style.transform = "translateX(100px)";
-    }, 500);
-
-    setTimeout(() => {
-      button.style.transform = originalPosition;
-      isMoving = false;
-    }, 2000); // Revenir à la position d'origine après 30 secondes
-  }
-}
-
-function moveTL(element) {
-  if (!isMoving) {
-    const originalPosition = element.style.transform;
-    element.style.transform = "translateY(-100px)";
-
-    setTimeout(() => {
-      element.style.transform = "translateX(-100px)";
-    }, 500);
-
-    setTimeout(() => {
-      element.style.transform = originalPosition;
-    }, 2000); // Revenir à la position d'origine après 2 secondes
-  }
-}
-
-function moveTY() {
-  if (!isMoving) {
-    isMovingTY = true;
+  if (!isMovingTd) {
+    isMovingTd = true;
     const button = document.getElementById("output");
     const originalPosition = button.style.transform;
 
@@ -60,11 +105,11 @@ function moveTY() {
 
     setTimeout(() => {
       button.style.transform = "translateX(-100px)";
-    }, 500);
+    }, 5000);
 
     setTimeout(() => {
       button.style.transform = originalPosition;
-      isMovingTY = false;
-    }, 2000); // Revenir à la position d'origine après 30 secondes
+      isMovingTd = false;
+    }, 2000);
   }
 }
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/calculator/",
-  build: {
-    rollupOptions: {
-      output: {
-        input: "index.html",
-        entryFileNames: "main.js", // Personnalisez le nom du fichier ici
-      },
-    },
-  },
 });
